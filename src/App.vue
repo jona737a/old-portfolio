a79a3a5378da3838decc115e587f3565762ad24e
unknown <jonathansmadsen@gmail.com>
2020-04-27 09:22:24 +0200

    1.

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,53 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-content>
+      <router-view></router-view>
+      
+    </v-content>
+  </v-app>
 </template>
 
+<script>
+
+export default {
+  name: 'App',
+
+};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@font-face {
+  font-family: 'TommyBold';
+  src:  url(./assets/fonts/MADETommySoftBoldPERSONALUSE.otf) format("opentype");
 }
 
-#nav {
-  padding: 30px;
+@font-face {
+  font-family: 'TommyBoldOutline';
+  src: url(assets/fonts/MADETommySoftBoldOutlinePERSONALUSE.otf) format("opentype");
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+@font-face {
+  font-family: 'TommyRegular';
+  src: url(./assets/fonts/MADETommySoftRegularPERSONALUSE.otf) format("opentype");
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+@font-face {
+  font-family: 'TommyMedium';
+  src: url(assets/fonts/MADETommySoftMediumPERSONALUSE.otf) format("opentype");
+}
+
+h4{
+  font-family: 'TommyMedium';
+  font-size: 93px;
+}
+h5{
+  font-family: 'TommyMedium';
+  font-size: 78.2px;
+}
+
+#box{
+  width: 100px;
+  height: 20px;
+  background-color: red;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,12 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <img src="gs://portfolio-3e0a9.appspot.com/Portfolio.png" alt="Portfolio">
+    <h4>Jonathan Evald</h4>
+    <h5>Stenholdt Madsen</h5>
+   
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
 </script>

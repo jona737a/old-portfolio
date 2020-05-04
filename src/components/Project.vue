7b7f9317ea7af0025ae6352418d862a465edfaa1
unknown <jonathansmadsen@gmail.com>
2020-05-04 11:14:11 +0200

    #4

---
+++ src/components/Project.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <router-link id="tilbage" to="/">Tilbage</router-link>
+    <h1> {{project.headline}} </h1>
+    <img id="banner" v-bind:src="project.imgLink" alt="Banner Image">
+    <v-row>
+      
+      <v-col cols="8" offset="2">
+        <v-row>
+          <h2>Informationer om projektet</h2>
+        </v-row>
+        <v-row>
+          <v-col cols="3" offset="2">
+            <p>Projektperiode:</p>
+            <p>Medvirkende:</p>
+            <p>Min Rolle:</p>
+            <p>Beskrivelse:</p>
+          </v-col>
+          <v-col cols="3">
+            <p>Projektperiode:</p>
+            <p>Medvirkende:</p>
+            <p>Min Rolle:</p>
+            <p>Beskrivelse:</p>
+          </v-col>
+          <v-col>
+            {{project.description}}
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    var project = this.$route.params
+    return{
+      show: false,
+      project:{
+        id: project.id,
+        headline: project.headline,
+        description: project.description,
+        imgLink: project.imgLink,
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+#tilbage{
+  text-decoration: underline;
+  color: black;
+  font-family: 'TommyRegular';
+  font-size: 32px;
+  margin-top: 100px;
+}
+h1{
+margin: 0;
+}
+
+#banner{
+  height: 800px;
+  width: 100%;
+  
+}
+
+h2{
+ margin: 0;
+ margin-top: 100px  ;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,57 +9,75 @@
     </div>
     <div id="main">
       <img class="parallax__layer parallax__layer--back" src="https://firebasestorage.googleapis.com/v0/b/portfolio-3e0a9.appspot.com/o/CV%20BG.png?alt=media&token=44676902-05a3-4732-864b-e2480f605b42" alt="Background">
-
-      <div id="work" v-for="(item, index) in projects" :key="index">
-        <img id="picture" v-bind:src=" item.imgLink ">
-      </div> 
+      <kinesis-container id="kinesis" v-for="(project, index) in projects" :key="index" v-bind:style="{marginLeft: project.random}">
+        <kinesis-element id="work" strength="2" type="depth">
+          <router-link id="link" v-bind:to="{ name: 'project', params: { 
+            id: project.id,
+            headline: project.headline,
+            description: project.description,
+            imgLink: project.imgLink,
+            }}" ><img id="picture" v-bind:src=" project.imgLink "> </router-link>
+        </kinesis-element> 
+      </kinesis-container>
       
     </div>
   </div>
 </template>
 
 <script>
-
-
-
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
 export default {
   data() {
     return {
       projects: [
         {
+          id: 1,
           headline: "homer one",
           description: "homer hungry description",
-          imgLink: "https://firebasestorage.googleapis.com/v0/b/portfolio-3e0a9.appspot.com/o/_MG_7140.jpg?alt=media&token=329a2790-4cef-4426-8edd-e699abfc39f9"
+          imgLink: "https://firebasestorage.googleapis.com/v0/b/portfolio-3e0a9.appspot.com/o/_MG_7140.jpg?alt=media&token=329a2790-4cef-4426-8edd-e699abfc39f9",
+          random: getRandomArbitrary(-10, 60) + "%"
         },
         {
+          id: 2,
           headline: "homer two",
           description: "homer really hungry description",
-          imgLink: "https://firebasestorage.googleapis.com/v0/b/portfolio-3e0a9.appspot.com/o/6d3.jpg?alt=media&token=798cb652-18ff-4977-8bf0-5226891a3d95"
+          imgLink: "https://firebasestorage.googleapis.com/v0/b/portfolio-3e0a9.appspot.com/o/6d3.jpg?alt=media&token=798cb652-18ff-4977-8bf0-5226891a3d95",
+          random: getRandomArbitrary(-10, 60) + "%"
         },
         {
+          id: 3,
           headline: "homer one",
           description: "homer hungry description",
-          imgLink: "https://firebasestorage.googleapis.com/v0/b/portfolio-3e0a9.appspot.com/o/_MG_7140.jpg?alt=media&token=329a2790-4cef-4426-8edd-e699abfc39f9"
+          imgLink: "https://firebasestorage.googleapis.com/v0/b/portfolio-3e0a9.appspot.com/o/_MG_7140.jpg?alt=media&token=329a2790-4cef-4426-8edd-e699abfc39f9",
+          random: getRandomArbitrary(-10, 60) + "%"
         },
         {
+          id: 4,
           headline: "homer one",
           description: "homer hungry description",
-          imgLink: "https://firebasestorage.googleapis.com/v0/b/portfolio-3e0a9.appspot.com/o/_MG_7140.jpg?alt=media&token=329a2790-4cef-4426-8edd-e699abfc39f9"
+          imgLink: "https://firebasestorage.googleapis.com/v0/b/portfolio-3e0a9.appspot.com/o/_MG_7140.jpg?alt=media&token=329a2790-4cef-4426-8edd-e699abfc39f9",
+          random: getRandomArbitrary(-10, 60) + "%"
         },
         {
+          id: 5,
           headline: "homer one",
           description: "homer hungry description",
-          imgLink: "https://firebasestorage.googleapis.com/v0/b/portfolio-3e0a9.appspot.com/o/_MG_7140.jpg?alt=media&token=329a2790-4cef-4426-8edd-e699abfc39f9"
+          imgLink: "https://firebasestorage.googleapis.com/v0/b/portfolio-3e0a9.appspot.com/o/_MG_7140.jpg?alt=media&token=329a2790-4cef-4426-8edd-e699abfc39f9",
+          random: getRandomArbitrary(-10, 60) + "%"
         },
       ]
-
     };
   }
 }
+
 </script>
 
 <style lang="scss">
 @import "./../scss/variables.scss";
+
+
 #container{
     perspective: 1px;
     height: 100vh;
@@ -81,14 +99,23 @@ export default {
   #main{
     display: flex;
     flex-flow: column;
-    align-items: center;
-    
   }
   .parallax__layer--back{
       z-index: 1;
       position: absolute;
       transform: translateZ(-3px) translateY(9000px) scale(5.5);
     }
+
+  #kinesis{
+    height: 800px;
+    width: 1000px;
+    margin-top: 400px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 2;
+    box-sizing: border-box;
+  }
   #work{
     z-index: 2;
     background-color: map-get($map: $colorz, $key: secondary);
@@ -99,24 +126,29 @@ export default {
     justify-content: center;
     align-items: center;
     margin: 0;
-    margin-top: 400px;
     
+    p{
+      z-index: 4;
+      position: relative;
+    }
   }
-  #work:nth-child(odd){
-    margin-left: random(70) / 100 * 100%;
   
-  }
-   #work:nth-child(even){
-    margin-left: random(70) / 100 * 100%;
-    
-  }
   #work:last-child{
     margin-bottom: 300px;
+  }
+  a#link{
+    height: 100%;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    box-sizing: border-box;
   }
   #picture{
     background-color: map-get($map: $colorz, $key: primary);
     height: 97%;
     width: 97%;
     clip-path: polygon(25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%, 0% 50%);
+    z-index: 3;
   }
 </style>
